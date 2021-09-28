f0e1609434ce0a712d2f61016f0b6be5f8bbeffd
ramonvax <ramonvax@gmail.com>
2021-09-28 19:04:22 -0300

    criado a tela inicial

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 /**
  * RockPaperScissors
  */
 public class RockPaperScissors {
     public static void main(String[] args) {
-        
+        int menu;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Seja bem vindo ao jogo\nPedra Papel e Tesoura");
+        System.out.println("Pressione Enter para continuar");
+        entrada.nextLine();
+        System.out.print("\033[H\033[2J"); 
+        System.out.println("1 - Jogar\n2 - Regras\n3 - Sair");
+        menu = entrada.nextInt();
+        System.out.print("\033[H\033[2J");
+        System.out.println("ok");
+        entrada.close();
     }    
 }
