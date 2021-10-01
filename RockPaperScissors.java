35f9f8b402377ce723cb29aa249fe82f0ee98fdc
ramonvax <ramonvax@gmail.com>
2021-09-30 22:01:13 -0300

    implementado o score

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,4 +1,3 @@
-import java.security.Principal;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,8 +6,9 @@ import java.util.Scanner;
  */
 public class RockPaperScissors {
     public static void main(String[] args) {
-        int menu;
-        boolean on;
+        int menu, userChoice, pcChoice, userScore, pcScore;
+        boolean on, startGame = false;
+        String[] game = { "pedra", "papel", "tesoura" };
         // MENU DE ENTRADA
         Scanner entrada = new Scanner(System.in);
         System.out.println("Seja bem vindo ao jogo\nPedra Papel e Tesoura");
@@ -18,7 +18,6 @@ public class RockPaperScissors {
         // MENU ESCOLHA
         on = true;
         while (on) {
-            Boolean startGame = false;
             System.out.println("1 - Jogar\n2 - Regras\n3 - Limpa Tela\n4 - Sair");
             menu = entrada.nextInt();
             // System.out.print("\033[H\033[2J");
@@ -40,52 +39,61 @@ public class RockPaperScissors {
                 System.out.println("Comando não existe\nTente novamente outr opção");
             }
             // JOGO
+            userScore = 0;
+            pcScore = 0;
             while (startGame) {
                 Random generator = new Random();
-                int userChoice;
-                int pcChoice = generator.nextInt(3);
-                String[] game = { "pedra", "papel", "tesoura" };
+                pcChoice = generator.nextInt(3);
+                System.out.printf("PLAYER : %d         |           PC : %d\n",userScore, pcScore);                
                 System.out.println("0 - Pedra   |   1 - Papel   |   2 - Tesoura");
-                System.out.println("7 - Limpa a Tela   |   9 - Retorna ao menu principal");
+                System.out.println("7 - Limpa a Tela   |    8 - Reseta Score    |   9 - Retorna ao menu principal");
                 userChoice = entrada.nextInt();
-                if (userChoice == 0 ||userChoice == 1 ||userChoice == 2) {
+                if (userChoice == 0 || userChoice == 1 || userChoice == 2) {
                     System.out.println("Voce escolheu: " + game[userChoice]);
                     System.out.println("Computador escolheu: " + game[pcChoice]);
                 }
                 // Escolhas
-                if (userChoice == pcChoice) {                    
+                if (userChoice == pcChoice) {
                     System.out.println("Empate");
                     System.out.println("\n\n");
-                } else if (userChoice == 0 && pcChoice == 1) {                    
+                } else if (userChoice == 0 && pcChoice == 1) {
+                    pcScore ++;
                     System.out.println("Você Perdeu");
                     System.out.println("Papel embrulha pedra");
                     System.out.println("\n\n");
-                } else if (userChoice == 0 && pcChoice == 2) {                    
+                } else if (userChoice == 0 && pcChoice == 2) {
+                    userScore ++;
                     System.out.println("Você Venceu");
                     System.out.println("Pedra quebra a tesoura");
                     System.out.println("\n\n");
                 } else if (userChoice == 1 && pcChoice == 2) {
+                    pcScore ++;
                     System.out.println("Você Perdeu");
                     System.out.println("Tesoura corta papel");
                     System.out.println("\n\n");
                 } else if (userChoice == 1 && pcChoice == 0) {
+                    userScore ++;
                     System.out.println("Você Venceu");
                     System.out.println("Papel embrulha pedra");
                     System.out.println("\n\n");
                 } else if (userChoice == 2 && pcChoice == 0) {
+                    pcScore ++;
                     System.out.println("Você Perdeu");
                     System.out.println("Pedra quebra a tesoura");
                     System.out.println("\n\n");
                 } else if (userChoice == 2 && pcChoice == 1) {
+                    userScore ++;
                     System.out.println("Você Venceu");
                     System.out.println("Tesoura corta papel");
                     System.out.println("\n\n");
-                } /* MENU */ 
+                } /* MENU */
                 else if (userChoice == 7) {
                     System.out.print("\033[H\033[2J");
-                } else if (userChoice == 8 ) {
-                    System.out.println("Menu sem atribuição");
-                } else if (userChoice == 9) {        
+                } else if (userChoice == 8) {
+                    System.out.println("Score resetado");
+                    pcScore = 0;
+                    userScore = 0;
+                } else if (userChoice == 9) {
                     startGame = false;
                     System.out.print("\033[H\033[2J");
                     System.out.println("Usuario saiu do jogo");
